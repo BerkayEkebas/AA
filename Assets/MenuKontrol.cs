1b012bf2935b6a0a92aab0ee19d774c15b75b26b
BerkayEkebas <48918056+BerkayEkebas@users.noreply.github.com>
2021-05-30 17:57:44 +0300

    Add files via upload

---
+++ Assets/MenuKontrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuKontrol : MonoBehaviour 
+{
+	public void yeniOyun()
+	{
+		    PlayerPrefs.DeleteAll ();
+			SceneManager.LoadScene ("1");
+
+	}
+	public void oyunaGit()
+	{
+		int kayitiLevel = PlayerPrefs.GetInt ("kayit");
+		if (kayitiLevel == 0) {
+			SceneManager.LoadScene (kayitiLevel + 1);
+		} 
+		else {
+			SceneManager.LoadScene (kayitiLevel);
+		}
+
+	}
+	public void cik()
+	{
+		Debug.Log ("Çıkış");
+		Application.Quit ();
+	}
+
+}
